ae3fd1e2410f72f1f27e49234f1ca6749d0c9603
Chris McGonigle <cmcgonigle1978@yahoo.co.uk>
2022-02-28 16:25:09 +0000

    Add basic footer to base template

--- forum/views.py
+++ forum/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 def homepage(request):
-    return render (request, 'base.html')
+    return render (request, 'home.html')
 
 def threads(request):
     return render (request, 'threads.html')
