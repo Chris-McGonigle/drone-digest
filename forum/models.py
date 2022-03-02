11d0adb0b5174c6afad7e19e35e40965ccab1967
Chris McGonigle <cmcgonigle1978@yahoo.co.uk>
2022-03-02 15:53:11 +0000

    Add class to create user posts

--- forum/models.py
+++ forum/models.py
@@ -1,4 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.deletion import CASCADE
+
+
+# Class to create unique threads
 
 class Thread(models.Model):
     name = models.CharField(max_length=150)
@@ -9,3 +14,14 @@ class Thread(models.Model):
     def __str__(self):
         return self.name
 
+# Class to create individual user posts      
+
+class Post(models.Model):
+    author = models.ForeignKey(User, on_delete=models.cascade)
+    thread = models.ForeignKey(Thread, on_delete=models.cascade)
+    body = models.TextField()
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.body[0:50]
--- forum/views.py
+++ forum/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 
+
 def homepage(request):
     return render (request, 'home.html')
+
 
 def threads(request):
     return render (request, 'threads.html')
