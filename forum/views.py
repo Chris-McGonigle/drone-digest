5804c7be065eba7335d684c27238ab71ad9901de
Chris McGonigle <cmcgonigle1978@yahoo.co.uk>
2022-03-04 15:32:20 +0000

    Link form to database

--- forum/views.py
+++ forum/views.py
@@ -1,21 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Thread, Post, Topic
 from .forms import ThreadForm 
 
+# Renders homepage view
 
 def homepage(request):
     threads = Thread.objects.all()
     context = {'threads': threads}    
     return render(request, 'home.html', context)
 
+# Renders list of threads
 
 def threads(request, pk):
     thread = Thread.objects.get(id=pk)
     context = {'thread': thread}        
     return render(request, 'threads.html', context)
 
+# Renders new thread and sends input to back end
 
 def newThread(request):
     thread = ThreadForm()
+    if request.method == "POST":
+        thread = ThreadForm(request.POST)
+        if thread.is_valid():
+            thread.save()
+            return redirect('homepage')
     context = {'thread': thread}
     return render(request, 'new-thread.html', context) 
