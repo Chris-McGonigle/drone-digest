2f71e71afd026db1f2f0d9c53c3a776d3f1d78b4
Chris McGonigle <cmcgonigle1978@yahoo.co.uk>
2022-03-14 17:31:24 +0000

    Add styling to homepage

--- static/js/script.js
+++ static/js/script.js
@@ -3,16 +3,18 @@
  * links on mobile devices
  */
 
+var browse = document.getElementById("subject-filter");
+
 
 function openBrowse() {
-    let browse = document.getElementById("subject-filter");
     browse.style.display = 'flex';
     browse.style.flexDirection = 'column';
     browse.style.flexWrap = 'wrap';
     browse.style.width = '80%';
     browse.style.position = 'fixed';
-    browse.style.backgroundColor = 'blue';
+    browse.style.backgroundColor = '#364958';
     browse.style.alignContent = 'center';
+    browse.style.borderRadius = '25px'
     browse.style.left = '50%';
     browse.style.top = '50%';
     browse.style.transform = 'translate(-50%, -50%)';
