c899f988b52d5d07fa2419c0699838e4632ebb3a
Chris McGonigle <cmcgonigle1978@yahoo.co.uk>
2022-03-16 09:50:06 +0000

    Fix styling of filter pop up on smaller screens

--- static/js/script.js
+++ static/js/script.js
@@ -10,10 +10,11 @@ function openBrowse() {
     browse.style.display = 'flex';
     browse.style.flexDirection = 'column';
     browse.style.flexWrap = 'wrap';
-    browse.style.width = '60%';
+    browse.style.width = '80%';
     browse.style.position = 'fixed';
     browse.style.backgroundColor = '#364958';
     browse.style.alignContent = 'center';
+    browse.style.border = '2px solid #ff5400;'
     browse.style.borderRadius = '25px'
     browse.style.left = '50%';
     browse.style.top = '50%';
