eccc1ea24311a3cc38895c165959157edc6a6632
Chris McGonigle <cmcgonigle1978@yahoo.co.uk>
2022-03-16 17:36:52 +0000

    Amend CSS and admin panel view

--- forum/admin.py
+++ forum/admin.py
@@ -2,10 +2,15 @@ from django.contrib import admin
 from .models import Thread, Post, Topic
 from django_summernote.admin import SummernoteModelAdmin
 
-@admin.register(Post)
-class PostAdmin(SummernoteModelAdmin):
+@admin.register(Thread)
+class ThreadAdmin(SummernoteModelAdmin):
 
     summernote_fields = ('description')
+    list_filter = ('subject', 'created')
 
-admin.site.register(Thread)
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+
+    list_filter = ('thread', 'created')
+
 admin.site.register(Topic)
